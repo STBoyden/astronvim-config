ba4e7a5f1521aa147b3c9593deadeb4926808335
STBoyden <sam@stboyden.com>
2023-06-20 08:09:02 +0100

    Add treesitter enure_installed

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "rust" },
+    ensure_installed = {
+      "lua",
+      "rust",
+      "go",
+      "bash",
+      "norg",
+      "norg_meta",
+    },
   },
 }
