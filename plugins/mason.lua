39346479add0da921dfc6d01538543afd694b382
STBoyden <sam@stboyden.com>
2023-08-02 13:38:44 +0100

    Use gofumpt and clang-format, ensure clangd installed by default

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "rust_analyzer" },
+      ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,7 +13,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      ensure_installed = { "prettier", "stylua", "rustfmt" },
+      ensure_installed = { "prettier", "stylua", "rustfmt", "clang-format" },
     },
   },
   {
