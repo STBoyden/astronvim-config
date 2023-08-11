d56cd1568049f336934e32c764ad0c0ca02502db
STBoyden <sam@stboyden.com>
2023-08-11 21:23:11 +0100

    Set spell checking to be on by default

--- options.lua
+++ options.lua
@@ -3,22 +3,22 @@ return {
   opt = {
     -- set to true or false etc.
     relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
+    number = true,         -- sets vim.opt.number
+    spell = true,          -- sets vim.opt.spell
+    signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
+    wrap = false,          -- sets vim.opt.wrap
     guifont = { "FantasqueSansM Nerd Font", ":h12" },
     list = true,
     listchars = { tab = "--", multispace = "_", trail = "#" },
   },
   g = {
     -- general astronvim configuration
-    mapleader = " ", -- sets vim.g.mapleader
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    mapleader = " ",                 -- sets vim.g.mapleader
+    autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true,              -- enable completion at start
+    autopairs_enabled = true,        -- enable autopairs at start
+    diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     --
     -- plugins configuration, using global variables
@@ -28,6 +28,8 @@ return {
     -- undo-tree
     undotree_WindowLayout = 3,
     undotree_SetFocusWhenToggle = 1,
+
+    spellang = "en_gb",
   },
 }
 -- If you need more control, you can use the function()...end notation
